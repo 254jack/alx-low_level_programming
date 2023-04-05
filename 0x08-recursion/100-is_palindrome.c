00d32882c07dd5b089a56b84baba4c2f155c3f49
254jack <taixonj@gmail.com>
2023-04-05 11:33:03 -0700

    add 100-is_palindrome.c file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_palindrome - is a function that returns a palindrome
+ * @s: string value
+ *
+ * Return: 1 in success
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (0);
+}
