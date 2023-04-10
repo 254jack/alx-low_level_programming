b0b18fdb4a8a267999f1a6e64359c9d16ea2c7e4
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:09:45 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -3,14 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _strspn - function that locates a char in a string
- * @s: first value - char
- * @accept: second value - char
- *
- * Return: char with result
- */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	return (s, accept);
+	return strspn(s, accept);
 }
