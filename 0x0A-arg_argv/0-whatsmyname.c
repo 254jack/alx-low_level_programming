ee9045d1e2b29e39626fb8eddc3bc2e7a75ae518
254jack <taixonj@gmail.com>
2023-04-10 13:48:18 -0700

    add 0-whatsmyname.c file

---
+++ 0x0A-arg_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that writes its name
+ * @argc: arg count
+ * @argv: array of pointers to arg strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%\n", argv);
+	return (0);
+}
