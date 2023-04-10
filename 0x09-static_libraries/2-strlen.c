751fd8c384881518f78b977ff780b7bc0b935468
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 18:25:48 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _strlen - function that locates a char in a string
+ * @s: first value - char
+ *
+ * Return: char with result
+ */
 int _strlen(char *s)
 {
 	return strlen(s);
