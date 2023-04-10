f676ae35a1d72aadff2cc2402da0af27c6878dfa
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 22:35:57 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,1 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-/**
-* _isupper - functions that returns an upper alphabet
-* @c: int
-*
-* Return: 0
-*/
-int _isupper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
+
