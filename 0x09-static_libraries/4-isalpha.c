0a1c620e7ea69136f5f298b1f1c24243c6f2fc87
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 20:31:14 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _strchr - function that locates an alphabet
+ * @c: second value - char
+ *
+ * Return: char with result
+ */
 int _isalpha(int c)
 {
-	return isalpha(c);
+	return (c);
 }
