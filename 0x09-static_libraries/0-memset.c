15822bd5f951508760dec494dfa65da8709ad713
254jack <taixonj@gmail.com>
2023-04-10 06:25:04 -0700

    add 0-memset.c file

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -3,8 +3,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _memset - function with args
+ * @s: strin value
+ * @b: variable
+ * @n: unsigned int
+ *
+ * Return: 0
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	return memset(s, b, n);
+	return (memset(s, b, n));
 }
