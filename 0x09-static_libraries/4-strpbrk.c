1e54e762b42c4034563bb8108ed77c11a3750402
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:10:29 +0300

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -3,14 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _strpbrk - function that locates a char in a string
- * @s: first value - char
- * @accept: second value - char
- *
- * Return: char with result
- */
+
 char *_strpbrk(char *s, char *accept)
 {
-	return (s, accept);
+	return strpbrk(s, accept);
 }
