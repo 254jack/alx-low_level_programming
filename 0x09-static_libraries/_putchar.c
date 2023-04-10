209aafb70bc4b6c46df2dc459b7d90ba928ba377
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 20:37:25 +0300

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+/**
+ * _strchr - function that locates a char in a string
+ * @c: value - char
+ *
+ * Return: char with result
+ */
 int _putchar(char c)
 {
-	return putchar(c);
+	return (c);
 }
