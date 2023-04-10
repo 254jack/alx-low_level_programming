62a92b04a1e1b73f0c379ca5e0d62058a41c38fb
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:11:05 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -3,14 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _strstr - function that locates a char in a string
- * @haystack: first value - char
- * @needle: second value - char
- *
- * Return: char with result
- */
+
 char *_strstr(char *haystack, char *needle)
 {
-	return (haystack, needle);
+    return strstr(haystack, needle);
 }
