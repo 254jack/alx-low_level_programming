864567af1249683d745e9fd1ad0cecad799fa71b
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:09:02 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -3,14 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _strchr - function that locates a char in a string
- * @s: first value - char
- * @c: second value - char
- *
- * Return: char with result
- */
+
 char *_strchr(char *s, char c)
 {
-	return (s, c);
+    return strchr(s, c);
 }
