0dad35067e488faf2231c5dacbfb65753888f11d
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 22:42:20 +0300

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,15 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
-/**
- * _putchar - function that puts a char in a string
- * @c: value - char
- *
- * Return: char with result
- */
+#include <string.h>
+
 int _putchar(char c)
 {
-	return (c);
+	return putchar(c);
 }
