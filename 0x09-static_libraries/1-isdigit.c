dab4fc5f1c15e03449edbef9f45e55a48d10b57b
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 22:47:50 +0300

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -3,13 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _isdigit - function that returns a digit
- * @c: second value - char
- *
- * Return: char with result
- */
+
 int _isdigit(int c)
 {
-	return (c);
+	return isdigit(c);
 }
