82d0eeed66b2d99a715f5d141db40251d026da75
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:04:53 +0300

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -3,14 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
-* _strncat - a function that concatenates two strings
-*@dest: output dest
-*@src: input source
-*@n: variable
-*Return: 0
-*/
+
 char *_strncat(char *dest, char *src, int n)
 {
-	return (dest, src, n);
+	return strncat(dest, src, n);
 }
