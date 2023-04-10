a095a676e4e6fcaebb06fe70e9c32d271e6377a7
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 18:21:36 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+/**
+ * _atoi - function that locates a  in a string
+ * @s: first value - char
+ *
+ * Return: 0
+ */
 int _atoi(char *s)
-
 {
 	return (s);
 }
