47a0e931db9846b7eacd517fb1ded083fd72ce9b
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 18:27:47 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -3,8 +3,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _strncpy - function that copies a string
+ * @dest: first value - char
+ * @src: second value - char
+ * @n: integer
+ *
+ * Return: 0
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	return strncpy(dest, src, n);
+	return (dest, src, n);
 }
