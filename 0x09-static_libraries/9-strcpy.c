96d22d39a9c9098af54266a61e81e3e11f7ed5b9
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:01:56 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -3,14 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _strcpy - function that copies a string
- * @dest: first value - char
- * @src: second value - char
- *
- * Return: char with result
- */
 char *_strcpy(char *dest, char *src)
 {
-	return (dest, src);
+    return strcpy(dest, src);
 }
