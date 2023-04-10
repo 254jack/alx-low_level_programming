6fa03e17ae1cfc053d1e25548a8747d54f7fcc08
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-10 23:08:23 +0300

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -3,15 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _memcpy - function that copies memory area
- * @dest: the address of memory to print
- * @src: second value
- * @n: third value
- *
- * Return: char with reusult of memset
- */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return (dest, src, n);
+    return memcpy(dest, src, n);
 }
