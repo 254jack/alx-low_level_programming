40bf694313ae7098a6243534be8db60f25948701
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-28 11:24:37 +0300

    create 1-list_len.c file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - a function that returns the number of elements in a linked list
+ * @h: pointer to the list_t
  *
- * Return: number of elements in h
+ * Return: 0
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		n++;
+		i++;
 		h = h->next;
 	}
-	return (n);
+	return (i);
 }
