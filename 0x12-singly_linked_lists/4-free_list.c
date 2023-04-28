8df421cde650630ce4e1d6e51e6cd98ebd3722ce
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-04-28 11:36:31 +0300

    add 4-free_lists.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list - a function that frees a linked list
+ * @head: the list to be freed
  */
 void free_list(list_t *head)
 {
