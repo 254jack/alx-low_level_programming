2afd27e25c3dd5e08b4df7c8ba0426a18db5db08
254jack <taixonj@gmail.com>
2023-04-30 11:27:19 -0700

    create 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node in a linked list,
+ * insert_nodeint_at_index - a function that inserts a new node
  * at a given position
  * @head: pointer to the first node
  * @idx: index of added first node
