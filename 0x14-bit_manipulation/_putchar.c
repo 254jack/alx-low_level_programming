10f5143c4127f8dc9d0ccd9dab9e949df0a924c2
254jack <taixonj@gmail.com>
2023-05-04 13:33:05 -0700

    create _putchar.c file

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar -a function that writes the character c to stdout
+ * @c: character
+ *
+ * Return: 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
