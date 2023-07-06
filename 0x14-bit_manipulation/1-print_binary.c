70be9c8203d3860947bb38cf953254bf0f98cfd8
254jack <taixonj@gmail.com>
2023-07-06 06:52:09 -0700

    create 1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_binary - a function that prints the binary equivalent of 
@@ -25,7 +24,5 @@ void print_binary(unsigned long int n)
 	}
 	if (!count)
 		_putchar('0');
-
-	_putchar('\n);
 
 }
