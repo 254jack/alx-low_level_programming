84ea26ebee0133762f3cc72fb0639f0f3dd0b367
254jack <taixonj@gmail.com>
2023-07-06 07:22:44 -0700

    create 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit - a function that returns the value of a bit 
- * at an index in a decimal number
+ * get_bit - a function that returns the value of a bit at an index in a decimal number
  * @n: number
  * @index: index
  *
