d08e44ff4a4236477c4d83efa52f0f66efa68dd2
254jack <taixonj@gmail.com>
2023-07-10 12:09:38 -0700

    create 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	jk = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (jk < 0)
 		return (-1);
